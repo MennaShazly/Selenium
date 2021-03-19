80dabe8f54f66b364b763386d2bc0f6a93d0a864
Menna Shazly <mnnshazly@gmail.com>
2021-03-19 16:13:18 +0200

    added

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import page
 from selenium import webdriver
 
 
-class Test(unittest.TestCase):
+class Tests(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
         self.driver.maximize_window()
-
-
-def test_register(self):
-        self.driver.get("http://127.0.0.1:5000/register")
+        self.driver.get("http://127.0.0.1:5000/")
         self.driver.implicitly_wait(50)
+
+
+    def test_register(self): #new student
+        mypage1 = page.HomePage(self.driver)
+        mypage1.click_regist()
         mypage = page.RegisterPage(self.driver)
         mypage.enter_name("Menna Shazly")
         mypage.enter_id("181037")
@@ -25,34 +27,34 @@ def test_register(self):
         mypage.click_regist_btn()
 
 
+
     def test_attendance(self):
-        self.driver.get("http://127.0.0.1:5000/attendance")
-        self.driver.implicitly_wait(50)
+        mypage1 = page.HomePage(self.driver)
+        mypage1.click_attendance()
         mypage = page.AttendancePage(self.driver)
-        mypage.enter_lec_num("3")
-        mypage.enter_code("CS321")
-        #mypage.upload_btn("Testing\Bill_Gates.jpg")
-        #self.driver.implicitly_wait(1000)
-        #mypage.click_submit()
+        mypage.enter_lec_num("3") #edit this
+        mypage.enter_code("CS321") #edit this
+        mypage.upload_btn("Bill_Gates.jpg") #edit this
+        self.driver.implicitly_wait(1000)
+        mypage.click_submit()
 
-    def test_admin(self):
-        self.driver.get("http://127.0.0.1:5000/admin/login")
-        self.driver.implicitly_wait(50)
+
+    def test_admin(self):  # right admin
+        mypage1 = page.HomePage(self.driver)
+        mypage1.click_admin()
         mypage = page.AdminLoginPage(self.driver)
-        mypage.enter_username("Sherlock")
-        mypage.enter_pass("sherlock221B221B")
+        mypage.enter_username("mosa") #edit this
+        mypage.enter_pass("mosa123456789")  #edit this
         mypage.check_show_pass()
-        mypage.click_login_btn()  
+        mypage.click_login_btn()
+        #assert redirecting 
 
-    def test_nav(self):
-        self.driver.get("http://127.0.0.1:5000/")
-        self.driver.implicitly_wait(50)
-        mypage = page.HomePage(self.driver)
-        #mypage.click_regist()
-        mypage.click_attendance()
-        mypage.click_contact()
-        mypage.click_admin()
-        mypage.click_home()
+
+    def test_contact(self):
+        mypage1 = page.HomePage(self.driver)
+        mypage1.click_contact()
+        assert "contact" in self.driver.title
+       
 
     def tearDown(self):
         self.driver.close()
